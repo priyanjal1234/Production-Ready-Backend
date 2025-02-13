aefc8ba6a9043cb621120eb781bcd27f3ea4e049
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-02-13 13:26:07 +0530

    Added cookie parser cors and express, made changes in app.js, handled db connection promise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import db from "./db/index.js";
+import app from './app.js';
 
 db()
+.then(function() {
+    app.listen(process.env.PORT || 4000,function() {
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch(function(err) {
+    console.log(`Connection with Mongodb failed ${err}`)
+})
